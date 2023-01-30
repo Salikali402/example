9d86b30a9d782151aa9301c45aeb90cae301f0ec
Salik Ali <salikali402@gmail.com>
2023-01-30 19:50:12 +0530

    first game

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Welcome to my Tic Tac Toe");
+
+// let turn= "X"
+
+// //Function to change the turn
+// cont changeTurn(){
+//     return turn ==="X"?"0":"X";
+// }
+
+var btn= document.querySelector(".button");
+btn.addEventListener("click", ()=>{
+    console.log("Hello");
+})
